2c94bf2a4ac69bcf8ca318ad62750636509b8834
bdinh <baodinh96@gmail.com>
2017-12-16 23:52:48 -0800

    inital home page refactor

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+{/*<nav className="navbar navbar-expand-md fixed-top navbar-light">*/}
+    {/*<a className="navbar-brand" href="#home">@baohdinh</a>*/}
+    {/*<button className="navbar-toggle navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">*/}
+        {/*<span className="navbar-toggler-icon"/>*/}
+    {/*</button>*/}
+    {/*<div className="collapse navbar-collapse" id="navbarSupportedContent">*/}
+        {/*<ul className="navbar-nav ml-auto">*/}
+            {/*<li className="nav-item "><a className="nav-link" href="#about">About <span className="sr-only">(current)</span></a></li>*/}
+            {/*<li className="nav-item"><a className="nav-link" href="projects.html">Projects</a></li>*/}
+            {/*<li className="nav-item"><a className="nav-link" href="photography.html">Photography</a></li>*/}
+        {/*</ul>*/}
+    {/*</div>*/}
+{/*</nav>*/}
