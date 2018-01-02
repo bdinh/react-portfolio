682b352daa84052ea14703d8525800c13a8e0231
bdinh <baodinh96@gmail.com>
2018-01-01 22:56:04 -0800

    Refactored landing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 import './include/bootstrap';
-import FontAwesome from 'react-fontawesome';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
+// import FontAwesome from 'react-fontawesome';
+// import '../node_modules/font-awesome/css/font-awesome.min.css';
+import Landing from './components/landing';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <div className="landing-strip">
-                    <p className="landing-header">Software Developer & Photographer</p>
-                    <p className="landing-subheader">
-                        Whether it's in my code or my photos,
-                        attention to detail is what I always strive for.
-                    </p>
-                    <div className="page-scroll">
-                        <a className="page-scroll-text" href="#">
-                            Learn More
-                            <br/>
-                            <FontAwesome
-                                name="chevron-down"
-                            />
-                        </a>
+                <Landing
+                    headerText={"Software Developer & Photographer"}
+                    subheaderText={"Whether it's in my code or my photos,\n" +
+                    "                    attention to detail is what I always strive for."}
+                />
+                <div>
+                    <p>Projects</p>
+                    <div className="project-container">
+                        <div className="project">
+
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ src/components/landing.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import FontAwesome from 'react-fontawesome';
+import '../../node_modules/font-awesome/css/font-awesome.min.css';
+
+export default class Landing extends Component {
+    render() {
+        const {
+            headerText,
+            subheaderText,
+            pageScrollText,
+        } = this.props;
+
+        return (
+            <div className="landing-strip">
+                <p className="landing-header">{headerText}</p>
+                <p className="landing-subheader">
+            gi        {subheaderText}
+                </p>
+                <div className="page-scroll">
+                    <a className="page-scroll-text" href="#">
+                        {pageScrollText ? pageScrollText : "Learn More"}
+                        <br/>
+                        <FontAwesome
+                            name="chevron-down"
+                        />
+                    </a>
+                </div>
+            </div>
+        )
+    }
+
+}
